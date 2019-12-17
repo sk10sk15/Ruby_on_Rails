a1346099d7be39a4c9629977d9254151a57750fc
sk10sk15 <sk25sk25_1015@icloud.com>
2019-12-17 18:04:18 +0900

    routes.rbを編集してディレクトリを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
+  get '/index' => 'users#index'
+  get '/show' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
