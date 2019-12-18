a1821dacf1dd9dafde1ebea2279b0cf794e87f09
sk10sk15 <sk25sk25_1015@icloud.com>
2019-12-18 18:57:36 +0900

    add new database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/index' => 'users#index'
-  get '/show' => 'users#show'
+  get '/show/:username' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
