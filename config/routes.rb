d74120254938cfa03d434700d99aa6cb57a85ae6
sk10sk15 <sk25sk25_1015@icloud.com>
2019-12-19 15:52:27 +0900

    finish わからないこと何個かもある

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+  get 'tweets/show'
+  get 'tweets/new'
+  post 'tweets' => 'tweets#create'
   get '/index' => 'users#index'
   get '/show/:username' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
